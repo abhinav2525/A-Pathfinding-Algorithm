4dbd68f0d79faa6c354ddee4131c338ed7b1bbbc
abhinav2525 <ec22064@qmul.ac.uk>
2023-11-22 23:38:41 +0000

    neighbor exists,heuristic,add neighbors,find low f

--- sketch.js
+++ sketch.js
@@ -1,33 +1,69 @@
-var cols = 5; // 5 x 5 grid
-var rows = 5; // 5 x 5 grid
+var cols = 25; // 5 x 5 grid
+var rows = 25; // 5 x 5 grid
 var grid = new Array(cols); // 2D array
 var openSet = []; // array of Spots
 var closedSet = []; // array of Spots
 var start; // Spot object
 var end; // Spot object
-
 var w, h; // width and height of each spot
+var path = []; // array of Spots
+
+// remove element from array
+function removeElementFromArray(arr, element) {
+  for (var i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] === element) {
+      arr.splice(i, 1);
+    }
+  }
+}
+
+function heuristic(a, b) {
+  // Euclidean distance
+  // var d = dist(a.i, a.j, b.i, b.j);
+  // Manhattan distance
+  var d = abs(a.i - b.i) + abs(a.j - b.j);
+  return d;
+}
 
 // Spot object
 class Spot {
   constructor(i, j) {
-
-    this.x = i; // x position
-    this.y = j; // y position
+    this.i = i; // i position
+    this.j = j; // j position
     this.f = 0; // f = g + h
     this.g = 0; // distance from start
     this.h = 0; // distance from end
-
+    this.neighbors = []; // array of Spot objects
+    this.previous = undefined; // previous Spot object
+    this.wall = false; // is it a wall?
 
     // show function
     this.show = function (color) {
       fill(color);
       noStroke(0);
-      rect(this.x * w, this.y * h, w - 1, h - 1); // -1 to see grid
+      rect(this.i * w, this.j * h, w - 1, h - 1); // -1 to see grid
+    };
+
+    // add neighbors
+    this.addNeighbors = function (grid) {
+      var i = this.i;
+      var j = this.j;
+      // check if neighbor exists
+      if (i < cols - 1) {
+        this.neighbors.push(grid[i + 1][j]);
+      }
+      if (i > 0) {
+        this.neighbors.push(grid[i - 1][j]);
+      }
+      if (j > 0) {
+        this.neighbors.push(grid[i][j - 1]);
+      }
+      if (j < rows - 1) {
+        this.neighbors.push(grid[i][j + 1]);
+      }
     };
   }
 }
-
 
 function setup() {
   // put setup code here
@@ -47,6 +83,13 @@ function setup() {
     }
   }
 
+  // add neighbors
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      grid[i][j].addNeighbors(grid);
+    }
+  }
+
   console.log(grid);
 
   // Start and end
@@ -60,8 +103,53 @@ function setup() {
 function draw() {
   // put drawing code here
   if (openSet.length > 0) {
-    // we can keep going
+    // find the lowest f
+    var lowestIndex = 0;
+    for (var i = 0; i < openSet.length; i++) {
+      if (openSet[i].f < openSet[lowestIndex].f) {
+        lowestIndex = i;
+      }
+    }
 
+    var current = openSet[lowestIndex];
+
+    if (current === end) {
+      console.log("DONE!");
+    }
+
+    // remove current from openSet
+    removeElementFromArray(openSet, current);
+    // add current to closedSet
+    closedSet.push(current);
+
+    // check neighbors
+    var neighbors = current.neighbors;
+
+    // loop through neighbors
+    for (var i = 0; i < neighbors.length; i++) {
+      var neighbor = neighbors[i];
+      // check if neighbor is not in closedSet
+      if (!closedSet.includes(neighbor)) {
+        var tempG = current.g + 1;
+        // check if neighbor is in openSet
+        if (openSet.includes(neighbor)) {
+          if (tempG < neighbor.g) {
+            neighbor.g = tempG;
+          }
+        } else {
+          neighbor.g = tempG;
+          openSet.push(neighbor);
+        }
+
+        // calculate heuristic
+        neighbor.h = heuristic(neighbor, end);
+        // calculate f
+        neighbor.f = neighbor.g + neighbor.h;
+
+        // set previous
+        neighbor.previous = current;
+      }
+    }
   } else {
     // no solution
   }
@@ -70,7 +158,7 @@ function draw() {
   // draw grid
   for (var i = 0; i < cols; i++) {
     for (var j = 0; j < cols; j++) {
-      grid[i][j].show(color(255));// show all spots
+      grid[i][j].show(color(255)); // show all spots
     }
   }
 
@@ -80,5 +168,20 @@ function draw() {
 
   for (var i = 0; i < openSet.length; i++) {
     openSet[i].show(color(0, 255, 0)); // show open set
+  }
+
+  // Find the path
+  path = [];
+
+  var temp = current; // last spot
+  path.push(temp);
+
+  // loop through previous
+  while (temp.previous) {
+    path.push(temp.previous);
+    temp = temp.previous;
+  }
+  for (var i = 0; i < path.length; i++) {
+    path[i].show(color(0, 0, 255)); // show path
   }
 }
